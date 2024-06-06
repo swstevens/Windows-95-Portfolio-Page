7537f4d37bc3d838fe3169f2bdf283ce80054ad8
swstevens <sstevensw@gmail.com>
2024-06-05 22:03:30 -0700

    tinkering with fonts

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <link rel="stylesheet" href="fonts.css" />
+    <body style="
+	font-family: Fit-VF;
+	font-style: normal;
+	font-weight: normal;
+	font-size: 72px;
+">hello</body>
+ </>
+ )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { Canvas } from '@react-three/fiber'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import "./fonts/Fit-Regular-Testing.woff"
+import "./fonts.css"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App/>
+)
