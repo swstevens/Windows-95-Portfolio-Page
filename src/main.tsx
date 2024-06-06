2b26d952a3bd81447deb7396aa51fe8b743e6c86
swstevens <sstevensw@gmail.com>
2024-06-05 21:32:13 -0700

    new skeleton

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,5 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Canvas
-    camera={{
-      fov: 45,
-      near: 0.1,
-      far: 2000,
-      position: [-3, 1.5, 4]
-    }}
-  >
-    <App />
-  </Canvas>
+  <App />  
 )
