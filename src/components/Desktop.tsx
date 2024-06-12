c80cfdd953b310d23f60b326e4291400279eadf8
swstevens <sstevensw@gmail.com>
2024-06-12 10:45:19 -0700

    start of work on multiple windows

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { act, useCallback, useState } from 'react';
 import DesktopShortcut from './DesktopShortcut';
 import Window from "./Window";
 import Toolbar from './Toolbar';
@@ -8,6 +8,7 @@ export interface DesktopProps {}
 
 const Desktop: React.FC<DesktopProps> = (props) => {
     const [isOpen, setIsOpen] = useState(true);
+    const [windows, setWindows] = useState<DesktopWindows>({});
     
     const [isMinimized, setIsMinimized] = useState(false);
     const [height, setHeight] = useState(window.innerHeight - 50 - 12);
@@ -36,18 +37,64 @@ const Desktop: React.FC<DesktopProps> = (props) => {
         setIsOpen(false)
     }
 
+    const addWindow = useCallback((key: string, element: JSX.Element) => {
+        setWindows((prevState) => ({
+            ...prevState,
+            [key]: {
+                zIndex: 1,
+                minimized: false,
+                component:  element,
+            },
+        }));
+    }, [])
+
+    const removeWindow = useCallback((key: string) => {
+        let activeWindows = { ...windows};
+        delete activeWindows[key];
+        setWindows(activeWindows);
+    }, [])
+
     return ( 
         <>
         <div style={styles.desktop} >
+            <div onMouseDown={() => addWindow('test',<Window width={width} height={height} top={top}
+            left={left} setOpen={() => removeWindow('test')} minimize={minimize} isMinimized={isMinimized}/>)}>
+            <DesktopShortcut
+                shortcutName={"Item 1"}
+            />
+            </div>
             <div onMouseDown={showWindow}>
             <DesktopShortcut
-                shortcutName={"banana"}
+                shortcutName={"Item 2"}
+            />
+            </div>
+            <div onMouseDown={showWindow}>
+            <DesktopShortcut
+                shortcutName={"Item 3"}
             />
             </div>
         </div>
         {isOpen && <Window width={width} height={height} top={top}
             left={left} setOpen={closeWindow} minimize={minimize} isMinimized={isMinimized}
         />}
+        {Object.keys(windows).map((key) => {
+            const element = windows[key].component;
+            return (
+                <div
+                    key={`win-${key}`}
+                    style={Object.assign(
+                        {},
+                        { zIndex: windows[key].zIndex },
+                        // windows[key].minimized && styles.minimized
+                    )}
+                >
+                    {React.cloneElement(element, {
+                        key,
+                        onClose: () => removeWindow(key),
+                    })}
+                </div>
+            );
+        })}
         <div className='too' style={styles.toolbar}>
             <Toolbar isMinimized={isMinimized} minimize={minimize} isOpen={isOpen}/>
         </div>
--- src/constants/Types.d.ts
+++ src/constants/Types.d.ts
@@ -1,3 +1,11 @@
 declare interface StyleSheetCSS {
     [key: string]: React.CSSProperties;
 }
+
+declare type DesktopWindows = {
+    [key in string]: {
+        zIndex: number;
+        component: React.ReactElement;
+        minimized: boolean;
+    };
+};
