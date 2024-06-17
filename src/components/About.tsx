a46a0a3c39118a2d16af73befca25635ce640466
Shea Stevens <63370427+swstevens@users.noreply.github.com>
2024-06-17 11:31:26 -0700

    start setting up unique pages

---
+++ src/components/About.tsx
@@ -0,0 +1,121 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <>
+        <h1 >WELCOME</h1>
+            <h3>I'm Shea Stevens</h3>
+            <br />
+            <div className="text-block">
+                <p>
+                    I'm a full stack software engineer looking for employment! I was born and raised in Hawaii, and in 2021 I graduated from 
+                    the University of Oregon
+                    with my BS in Computer Science. Minors in Mathematics and Japanese. 
+                </p>
+                <br />
+                <p>
+                    Thank you for taking the time to check out my portfolio. I
+                    really hope you enjoy exploring it as much as I enjoyed
+                    building it. If you have any questions or comments, feel
+                    free to contact me using{' '}
+                    <a href="mailto:sstevensw@gmail.com">
+                        sstevensw@gmail.com
+                    </a>
+                </p>
+            </div>
+            <div className="text-block">
+                <h3>About Me</h3>
+                <br />
+                {/* <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p> */}
+                <p>
+                    I've always been an avid tinkerer. From laptops to VCR's to robotics, my childhood was filled with taking things apart and seeing how they work. These days, I'm a coder. I love to do back end work, setting up the connections between all of the different systems in an environment. Whether it's a large monolith, or a swift microservice system, I'm always at home working through additions and optimizations.
+                </p>
+                <br />
+                <p>
+                    Most of my experience lies in coding backend API's, but I'm rapidly building skills to work as a Full Stack Developer. By far, I used Python the most. It may not be the fastest language out there, but its lenient syntax let's you spin a small project up the fastest. When speed is a priority, I like to use C# or C++. And of course, I've got all those algorithm skills that they teach you in university too (Djikstra eat your heart out).
+                </p>
+                <br />
+                {/* <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p> */}
+                <p>
+                   My passion for programming also comes from the love of video games, which have shaped a lot of who I am today. When I would pick up my controller and be amazed by what could be created through code, I knew deep down that this was my path in life. That early era of janky, low poly models is something that I hold dear to my heart, and something that I replicate in my own personal projects. It helps that you don't need a super large team like newer releases, either.
+                </p>
+                <br />
+                {/* <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p> */}
+                <br />
+                <br />
+                <div style={{}}>
+                    <div
+                        style={{
+                            flex: 1,
+                            textAlign: 'justify',
+                            alignSelf: 'center',
+                            flexDirection: 'column',
+                        }}
+                    >
+                        <h3>My Hobbies</h3>
+                        <br />
+                        {/* <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p> */}
+                        <p>
+                            Outside of programming, I've got quite a few hobbies. Flim photography is one. Nothing beats the physical mediums in my opnion, and the imperfections that come with film. You can check out some of my photographs here:
+                        </p>
+                        <br />
+                        <p>
+                            My personal projects tend to include some form of retro game. Recently, I've really liked the Godot Engine, due to it's open source and free nature. After the debacle with Unity liscensing, I've seen a lot of indie devs move there as well. Probably the most interesting thing about it is the language it uses, aptly named Godot, which is it's own fork of C#. Perfect for those smaller teams looking to build a retro labor of love.
+                        </p>
+                        <br />
+                        <p>
+                            And believe it or not, I'm quite a cook. While recipes can be quite formulaic, I try to keep my instincts in mind in the kitchen. Learn enough about the ingredients you cook with and you can come up with something pretty unique based on what's in your fridge. 
+                        </p>
+                    </div>
+                    <div style={styles.verticalImage}>
+                        <img src={'assets/cherry blossom 2024.jpg'} style={styles.image} alt="" />
+                        <p>
+                            <sub>
+                                <b>Figure 2:</b> Me, April 2024
+                            </sub>
+                        </p>
+                    </div>
+                </div>
+                <br />
+                <br />
+                <p>
+                    If you have any questions or comments I would love to hear
+                    them. You can reach me through the{' '}
+                    at{' '}
+                    <a href="mailto:sstevensw@gmail.com">
+                        sstevensw@gmail.com
+                    </a>
+                </p>
+                </div>
+            </>
+    )
+}
+
+const styles: StyleSheetCSS = {
+    image: {
+        height: 'auto',
+        width: '100%',
+        paddingTop: '10%',
+    },
+    verticalImage: {
+        alignSelf: 'center',
+        // width: '80%',
+        marginLeft: 32,
+        flex: 0.8,
+
+        alignItems: 'center',
+        // marginBottom: 32,
+        textAlign: 'center',
+        flexDirection: 'column',
+    },
+}
+
+export default About;
